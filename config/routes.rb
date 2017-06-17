ba8d11e79cc377444cccb29e31e0d24a1543acf7
kwaters12 <kellywaters@gmail.com>
2017-06-17 11:52:37 -0700

    Add RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   devise_for :users
   mount ForestLiana::Engine => '/forest'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
